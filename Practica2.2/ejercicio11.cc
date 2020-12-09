26881e0052f3b87dd1ec81b90f6ff61a5de985f4
Jesús <44319321+Yisas12@users.noreply.github.com>
2020-12-09 19:48:47 +0100

    MIRAR

---
+++ Practica2.2/ejercicio11.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/sysmacros.h>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char **argv){
+  struct stat s;
+  int intstat = stat(argv[1], &s);
+
+  char* hard = malloc(sizeof(char)*(5 + strlen(argv[1])));
+  char* sym = malloc(sizeof(char)*(5 + strlen(argv[1])));
+
+  strcpy(hard, argv[1]);
+  strcpy(sym, argv[1]);
+  hard = strcat(hard, ".hard");
+  sym = strcat(sym, ".sym");
+
+  cout << hard << " " << sym << "\n";
+
+  return 1;
+}
