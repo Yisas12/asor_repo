29183bf06de0f7efac34746688ef769f88fa5aca
Jesús <44319321+Yisas12@users.noreply.github.com>
2020-12-14 10:01:57 +0100

    v2

--- Practica2.2/ejercicio11.cc
+++ Practica2.2/ejercicio11.cc
@@ -12,9 +12,9 @@ using namespace std;
 int main(int argc, char **argv){
   struct stat s;
   int intstat = stat(argv[1], &s);
-
-  char* hard = malloc(sizeof(char)*(5 + strlen(argv[1])));
-  char* sym = malloc(sizeof(char)*(5 + strlen(argv[1])));
+  
+  char* hard =(char*) malloc(sizeof(char)*(5 + strlen(argv[1])));
+  char* sym = (char*)malloc(sizeof(char)*(5 + strlen(argv[1])));
 
   strcpy(hard, argv[1]);
   strcpy(sym, argv[1]);
@@ -25,3 +25,4 @@ int main(int argc, char **argv){
 
   return 1;
 }
+
