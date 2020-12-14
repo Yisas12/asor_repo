7c57d03cc15c99c75f09510a14f68be004f79224
Jesús <44319321+Yisas12@users.noreply.github.com>
2020-12-14 10:25:17 +0100

    v3

--- Practica2.2/ejercicio11.cc
+++ Practica2.2/ejercicio11.cc
@@ -12,17 +12,28 @@ using namespace std;
 int main(int argc, char **argv){
   struct stat s;
   int intstat = stat(argv[1], &s);
-  
-  char* hard =(char*) malloc(sizeof(char)*(5 + strlen(argv[1])));
-  char* sym = (char*)malloc(sizeof(char)*(5 + strlen(argv[1])));
 
-  strcpy(hard, argv[1]);
-  strcpy(sym, argv[1]);
-  hard = strcat(hard, ".hard");
-  sym = strcat(sym, ".sym");
+ // char* hard =(char*) malloc(sizeof(char)*(5 + strlen(argv[1])));
+ // char* sym = (char*)malloc(sizeof(char)*(5 + strlen(argv[1])));
 
-  cout << hard << " " << sym << "\n";
+  string hard = argv[1];
+  string sym = argv[1];
+  hard += ".hard";
+  const char* ha = hard.c_str();
+
+  sym+=".sym";
+  const  char * syml = sym.c_str();
+
+  int h = link(argv[1],ha);
+
+  int sy = link(argv[1], syml);
+
+ // strcpy(hard, argv[1]);
+ // strcpy(sym, argv[1]);
+ // hard = strcat(hard, ".hard");
+ // sym = strcat(sym, ".sym");
+
+  cout << h << " " << sy << "\n";
 
   return 1;
 }
-
