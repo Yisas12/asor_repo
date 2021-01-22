a194d438e7432458fc11b10361f356137e04466c
Jesús <44319321+Yisas12@users.noreply.github.com>
2021-01-22 19:30:40 +0100

    v1

---
+++ Practica2.5/Ejercicio9.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <signal.h>
+#include <errno.h>
+
+#include <time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+volatile int stop = 0;
+void z(int sen){
+
+  pid_t pid;
+
+  pid = wait(NULL);
+
+  printf("Saliendo pid\n");
+
+}
+
+int main(int argc, char **argv){
+
+  struct addrinfo hints;
+   struct addrinfo *res;
+
+  memset(&hints,0,sizeof(struct addrinfo));
+  hints.ai_family = AF_UNSPEC; //Coger direcciones IPv4 y Ipv6
+  hints.ai_socktype = SOCK_STREAM; //Tipo de socket TCP
+  hints.ai_flags = AI_PASSIVE; // node == NULL
+
+   if (getaddrinfo(argv[1], argv[2], &hints, &result) != 0) {
+      printf("ERROR: No se ha podido ejecutar el getaddrinfo.");
+      exit(EXIT_FAILURE);
+      }
+
+  int socket = socket(res->ai_family, res->ai_socktype, 0);
+  bind(socket, res->ai_addr, res->ai_addrlen);
+  freeaddrinfo(res);
+
+  listen(socket, 5);
+
+  struct sockaddr_storage cliente;
+  socklen_t clen = sizeof(cliente);
+  
+  char buffer[81];
+  int cli_sd;
+  char host[NI_MAXHOST];
+  char server[NI_MAXSERV];
+  ssize_t c;
+
+  signal(SIGCHLD, z);
+
+  int status;
+  
+  while (1) {
+
+    cli_sd = accept(socketTCP,(struct sockaddr *) &cli, &clen);
+
+    pid_t pid;
+    pid = fork();
+    if (pid == 0) {
+      while (1) {
+        getnameinfo((struct sockaddr *)&cli, clen, host, NI_MAXHOST, serv, NI_MAX_SERV, NI_NUMERICHOST);
+         printf("[PID: %i] Conexión desde %s:%s\n", getpid(), host, serv);
+
+        c = recv(cli_sd, buf, 80, 0);
+        buf[c] = '\0';
+
+        if ((buf[0] == 'q') && (c == 2)) {
+          printf("Conexión terminada\n");
+          exit(0);
+        } else {
+          send(cli_sd, buf, c, 0);
+        }
+      }
+    } else {
+      pid = wait(&status);
+      close(cli_sd);
+      exit(0);
+    }
+  }
+
+  close(cli_sd);
+  
+  return 0;
+  
+}
